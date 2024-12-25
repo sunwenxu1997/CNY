951bcea25f9985c45b6e7b8c04df9448ba5d148e
孙文旭 <wenxu.sun@opple.com>
2024-12-25 15:34:27 +0800

    优惠券小程序跳转页面标题修改

--- src/router/index.js
+++ src/router/index.js
@@ -60,7 +60,7 @@ const routes = [
     path: '/coupon',
     name: 'Coupon',
     component: () => import('@/views/coupon/index.vue'),
-    meta: { title: '好运签,跳转很丝滑~' }
+    meta: { title: '好运签' }
   },
   {
     path: '/404',
